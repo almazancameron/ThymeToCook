ca2b8a0f99357a8d9640bc8ec3e1ac7b881e1932
Cameron <almazancameron@gmail.com>
2022-09-06 18:28:04 -0500

    Basic code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
+import { Router, Route, Switch } from 'react-router-dom';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBsGQS4oEvSmDSABqYUTIIi9PXjyW5DH8c",
+  authDomain: "capstone-3-74587.firebaseapp.com",
+  projectId: "capstone-3-74587",
+  storageBucket: "capstone-3-74587.appspot.com",
+  messagingSenderId: "346403488737",
+  appId: "1:346403488737:web:65927b8a7a1725eb36c8a0",
+  measurementId: "G-1HE4GV7FX5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function testAddDoc() {
+  try {
+    const docRef = await addDoc(collection(db, "recipes"), {
+      name: 'Pizza'
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={testAddDoc}>test</button>
       </header>
     </div>
   );
