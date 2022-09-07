eecf2f6e392a7b1a7ab990a1e44eddb0026a9949
beerobinson <bee.robinson91@gmail.com>
2022-09-07 18:04:27 -0400

    Added button

--- src/pages/Recipes/Recipes.js
+++ src/pages/Recipes/Recipes.js
@@ -1,1 +1,16 @@
-Hello
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+
+export default function RecipesPage() {
+  return (
+    <RecipeButton/>
+  );
+}
+
+export const RecipeButton = () => {
+    
+      <Button variant="contained">Recipes</Button>
+   
+}
