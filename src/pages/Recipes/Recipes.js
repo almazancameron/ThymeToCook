2f024ca82c428ca40641aed1ebad8f19c568bd9b
beerobinson <bee.robinson91@gmail.com>
2022-09-07 21:57:45 -0400

    added spoonacular api

--- src/pages/Recipes/Recipes.js
+++ src/pages/Recipes/Recipes.js
@@ -1,58 +1,65 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Typography from '@mui/material/Typography';
-import NoBake from './assets/nobake.jpg';
-
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Typography from "@mui/material/Typography";
+import NoBake from "./assets/nobake.jpg";
 
 export default function RecipesPage() {
+    const [recipes, setRecipes] = useState([])
+
+    useEffect(() => {
+        const getRecipes = async () => {
+            try {
+                const list = await axios.get(`https://api.spoonacular.com/recipes/random?number=3&limitLicense=true&apiKey=${process.env.REACT_APP_API_KEY}`)
+                setRecipes(list.data.recipes)
+            } catch (e) {
+                console.log('ERROR GETTING RECIPES')
+            }
+        }
+        getRecipes()
+    }, [])
+
   return (
     <div>
-    <h1>Recipes</h1>
-    <RecipeCard/>
+      <h1>Recipes</h1>
+      {recipes.map((recipe) => {
+        return <RecipeCard key={recipe.id} recipe={recipe} />;
+      })}
     </div>
   );
 }
 
-const mockRecipes = [
-    {
-        name: "Granma's No Bake Cookies",
-        recipeId: "125nbc",
-        image:{NoBake},
-        alt:"No bake cookie on paper"
-    },
-    {
-        name: "Rice Bacon Cheese Casserole",
-        recipeId: "340way",
-        image:{NoBake},
-        alt:"No bake cookie on paper"
-    }
-]
+const RecipeCard = ({ recipe }) => {
+  const { title, image, summary } = recipe;
 
-export function RecipeCard(){
-  return(
+  const strip = (html) => {
+    let doc = new DOMParser().parseFromString(html, 'text/html');
+    return doc.body.textContent || "";
+ }
+
+  return (
     <Card sx={{ maxWidth: 275 }}>
       <CardContent>
-      <CardMedia
-        className='recipe-img'
-        component="img"
-        height="194"
-        image={NoBake}
-        alt="No bake cookie on paper"
-      />
-      <Typography className='recipe-name' sx={{ fontSize: 16 }}>
-          Grandma's No Bake Cookies
-      </Typography>
-      <Typography className='recipe-description' sx={{ fontSize: 12}}>
-       
-      </Typography>
-
+        <CardMedia
+          className="recipe-img"
+          component="img"
+          height="194"
+          image={image}
+          alt={title}
+        />
+        <Typography className="recipe-name" sx={{ fontSize: 16 }}>
+          {title}
+        </Typography>
+        <Typography className="recipe-description" sx={{ fontSize: 12 }}>
+          {strip(summary)}
+        </Typography>
       </CardContent>
     </Card>
   );
-}
+};
